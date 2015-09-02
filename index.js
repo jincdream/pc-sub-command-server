d9a9b8e13e6e220082a13065642b4026f76c492c
Jin_C <275727449@qq.com>
2015-09-02 17:14:25 +0800

    add proxy
    add proxy

--- index.js
+++ index.js
@@ -9,40 +9,41 @@ exports.usage = '<commad> [option]'
 exports.desc = 'open local server for liveaload and preview'
 
 exports.options = {
-	'-s, --start <port>': 'start a localserver with <port> || 8090',
-	'-x, --stop': 'stop the loacal sertver'
+  '-s, --start <port>': 'start a localserver with <port> || 8090',
+  '-x, --stop': 'stop the loacal sertver',
+  '-start, --start <port>': 'start a localserver with <port> || 8090',
+  '-stop, --stop': 'stop the loacal sertver'
 };
 exports.run = function(argv, cli, env){
-	if (argv.h || argv.help) {
+  if (argv.h || argv.help) {
     return cli.help(exports.name, exports.options);
   }
-	var command = argv._
-	if(argv.s){
-		// server(argv.s)
-		console.log(argv.s)
-		console.log('~~~~~~~')
-		// process._server_port_ = argv.s
-		var port = argv.s === true ? 8090 : argv.s
-		pm2.connect(function() {
-		  pm2.start({
-				watch: __dirname + '/server/index.js',
-				name: name,
-		    script    : __dirname + '/server/index.js',         // Script to be run
-		    exec_mode : 'fork',        // Allow your app to be clustered
-		    // instances : 4,           // Optional: Scale your app by 4
-				"cwd": process.cwd(),
-		    max_memory_restart : '100M',   // Optional: Restart your app if it reaches 100Mo
-				args: [port]
-		  }, function(err, apps) {
-				if(err) console.error(err)
-		    pm2.disconnect();
-		  });
-		});
-	}else if(argv.x){
-		pm2.connect(function() {
-			pm2.delete(name, function(err, apps) {
-				pm2.disconnect();
-			})
-		});
-	}
+  var command = argv._
+  if(argv.s || argv.start){
+    // server(argv.s)
+    // process._server_port_ = argv.s
+    var port = argv.s === true ? 8090 : argv.s
+    console.log('port:'+argv.s)
+    pm2.connect(function() {
+      pm2.start({
+        watch: __dirname + '/server/index.js',
+        name: name,
+        script    : __dirname + '/server/index.js',         // Script to be run
+        exec_mode : 'fork',        // Allow your app to be clustered
+        // instances : 4,           // Optional: Scale your app by 4
+        "cwd": process.cwd(),
+        max_memory_restart : '100M',   // Optional: Restart your app if it reaches 100Mo
+        args: [port]
+      }, function(err, apps) {
+        if(err) console.error(err)
+        pm2.disconnect();
+      });
+    });
+  }else if(argv.x || argv.stop){
+    pm2.connect(function() {
+      pm2.delete(name, function(err, apps) {
+        pm2.disconnect();
+      })
+    });
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,36 +1,196 @@
 var express = require('express')
 var path = require('path')
 var compress = require('compression')
+var http = require('http')
+var URL = require('url')
+var qs = require('querystring')
+var multer = require('multer')
+var router = express.Router()
+var bodyP = require('body-parser')
+
 var app = express()
 var _THIS_DIR = process.cwd()
 var _PORT = process.argv[2]
 console.log(_PORT)
-var _OUTPUT = path.join(_THIS_DIR,'/output/')
-var _DEV = path.join(_THIS_DIR,'/dev/')
-//通常 logErrors 来纪录诸如 stderr, loggly, 或者类似服务的错误信息：
+var _OUTPUT = path.join(_THIS_DIR, '/output/')
+var _DEV = path.join(_THIS_DIR, '/dev/')
+  //通常 logErrors 来纪录诸如 stderr, loggly, 或者类似服务的错误信息：
 
 var logErrors = function(err, req, res, next) {
-  console.error(err.stack);
-  next(err);
-}
-//clientErrorHandler 定义如下（注意错误非常明确的向后传递了）：
-
-var clientErrorHandler = function(err, req, res, next) {
-  if (req.xhr) {
-    res.status(500).send({ error: 'Something blew up!' });
-  } else {
+    console.error(err.stack);
     next(err);
   }
-}
-//下面的 errorHandler "捕获所有" 的异常，定义如下:
+  //clientErrorHandler 定义如下（注意错误非常明确的向后传递了）：
+
+var clientErrorHandler = function(err, req, res, next) {
+    if (req.xhr) {
+      res.status(500).send({
+        error: 'Something blew up!'
+      });
+    } else {
+      next(err);
+    }
+  }
+  //下面的 errorHandler "捕获所有" 的异常，定义如下:
 
 var errorHandler = function(err, req, res, next) {
   res.status(500);
-  res.render('error', { error: err });
+  res.render('error', {
+    error: err
+  });
 }
+
+app.use(bodyP.json())
+app.use(bodyP.urlencoded({
+    extended: !0
+  }))
+  // app.use(multer())
 app.use(compress())
+
+router.route('/proxy.json')
+  .all(function(req, res, next) {
+    res.set({
+      // 'Content-Type': 'text/plain',
+      // 'Content-Length': '123',
+      // 'ETag': '12345'
+      "Access-Control-Allow-Origin": "*"
+    })
+    console.log(req.query.url, req.body.url)
+    if (!req.query.url && !req.body.url) {
+      return res.end('这里并没有什么秘密噢。~喵！（带上参数才能进行代理啊魂蛋。）')
+    } else {
+      next()
+    }
+  }).get(function(req, res, next) {
+    console.log('get start')
+    var _url = 'http://' + req.query.url,
+      url
+    var callback = req.query.callback
+    delete req.query.url
+    if (!!~_url.indexOf('?')) url = _url + '&' + qs.stringify(req.query);
+    else url = _url + '?' + qs.stringify(req.query);
+    new Promise(function(resolve, reject) {
+      var pReq = http.get(url, function(pRes) {
+        var data = '',
+          pData
+
+        pRes.on('data', function(chunk) {
+          data += chunk
+        })
+        pRes.on('end', function(err) {
+          if (err) return reject({
+            error: "-1",
+            info: err
+          });
+          else {
+            data = data.replace(/[\r\n]*/g, '').replace(
+              /.*?\((\{.*?\})\)\;*/,
+              function() {
+                if (arguments[1]) return arguments[1];
+                return arguments[0];
+              })
+            try {
+              pData = JSON.parse(data)
+            } catch (e) {
+              var error = new Error(e)
+              return reject({
+                error: -1,
+                info: error,
+                jinc_say: 'json parse error',
+                parseError: data
+              })
+            }
+
+            if (callback) return resolve(['jsonp', pData])
+            else {
+              return resolve(['json', pData])
+            }
+          }
+        })
+      }).on('error', function(e) {
+        reject({
+          error: "-2",
+          info: e
+        });
+      })
+      pReq.end()
+    }).then(function(dataAry) {
+      res[dataAry[0]](dataAry[1])
+      console.log(dataAry)
+    }).catch(function(error) {
+      res.json(error)
+    })
+  }).post(function(req, res, next) {
+    console.log('post start')
+    var url = 'http://' + req.body.url
+    var option = URL.parse(url)
+    option.method = 'POST'
+    var headers = req.headers
+    option.headers = {
+      Host: option.host,
+      // 'Content-Length': +headers['content-length'],
+      'Origin': option.host,
+      Cookie: req.body.cookie
+    }
+    delete req.body.cookie
+    delete req.body.url
+    var postData = qs.stringify(req.body)
+    var _length = postData.length
+    option.headers['Content-Length'] = _length
+
+
+    new Promise(function(resolve, reject) {
+      var pReq = http.request(option, function(pRes) {
+        var data = '',
+          pData
+
+        pRes.on('data', function(chunk) {
+          data += chunk
+        })
+        pRes.on('end', function(err) {
+          if (err) return reject({
+            error: "-1",
+            info: err
+          });
+          else {
+            data = data.replace(/[\r\n]*/g, '').replace(
+              /.*?\((\{.*?\})\)\;*/,
+              function() {
+                if (arguments[1]) return arguments[1];
+                return arguments[0];
+              })
+            try {
+              pData = JSON.parse(data)
+            } catch (e) {
+              var error = new Error(e)
+              return reject({
+                error: -1,
+                info: error,
+                jinc_say: 'json parse error',
+                parseError: data
+              })
+            }
+            return resolve(['json', pData])
+          }
+        })
+      }).on('error', function(e) {
+        reject({
+          error: "-2",
+          info: e
+        });
+      })
+      pReq.write(postData || '')
+      pReq.end()
+    }).then(function(dataAry) {
+      res[dataAry[0]](dataAry[1])
+    }).catch(function(error) {
+      res.json(error)
+    })
+  })
+app.use(router)
+
 // app.use(express.static(_OUTPUT));
-app.use('/api/:project/:type/:api',function(req,res){
+app.use('/api/:project/:type/:api', function(req, res) {
   // var params = req.params.project.split('-')
   // var project = params[0]
   // var type = params[1]
@@ -40,21 +200,21 @@ app.use('/api/:project/:type/:api',function(req,res){
   var type = req.params.type
   var api = req.params.api
 
-  var file = path.resolve(_DEV,'./'+project,'./source/api/','./'+api)
+  var file = path.resolve(_DEV, './' + project, './source/api/', './' + api)
   var date = require(file).data
   res[type](date)
-  // console.log(params)
-  // res.end(file)
-  // data = null
+    // console.log(params)
+    // res.end(file)
+    // data = null
 })
 
 // app.use('/',express.static('./'))
-app.use('/',express.static(_OUTPUT))
-app.use('/dev/',express.static(_DEV))
-// app.get('/t',function(req,res){
-//   res.end(_PORT)
-// })
-app.get('/JinC',function(req,res){
+app.use('/', express.static(_OUTPUT))
+app.use('/dev/', express.static(_DEV))
+  // app.get('/t',function(req,res){
+  //   res.end(_PORT)
+  // })
+app.get('/JinC', function(req, res) {
   res.end('Bad Boy,Bad Boy.')
 })
 console.log(path.resolve('./'))
@@ -62,5 +222,5 @@ app.use(logErrors);
 app.use(clientErrorHandler);
 app.use(errorHandler);
 var server = app.listen(_PORT || 8090, function() {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
